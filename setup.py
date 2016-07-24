fd62ae8ea9510a0572fc46bcd1bee588a55435c6
tylerlaberge <tyler.laberge@maine.edu>
2016-07-24 17:47:30 -0400

    cleaned up setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
-setup(name='pypatterns',
-      version='0.1',
-      description='Python Design Patterns',
-      author='Tyler LaBerge',
-      packages=[
+setup(
+    name='pypatterns',
+    version='0.1',
+    description='Python Design Patterns',
+    author='Tyler LaBerge',
+    packages=[
           'pypatterns',
           'pypatterns.creational'
-      ],
-      )
+    ],
+)
