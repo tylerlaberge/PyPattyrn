08ac7fdb4bee2b4808c1d1d76e7a0bcc9535fb24
tylerlaberge <tyler.laberge@maine.edu>
2016-09-10 18:50:00 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='pypat',
-    version='1.0',
+    version='1.1',
     description='Python Design Patterns',
     author='Tyler LaBerge',
     author_email='tyler.laberge@maine.edu',
     url='https://github.com/tylerlaberge/PyPat',
-    download_url='https://github.com/tylerlaberge/PyPat/tarball/v1.0',
+    download_url='https://github.com/tylerlaberge/PyPat/tarball/v1.1',
     keywords=['design', 'pattern', 'patterns'],
     classifiers=[],
     packages=[
